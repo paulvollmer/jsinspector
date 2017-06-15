c3fe4d2508fa9d97863496e9334d23a6c566f578
Paul Vollmer <paul.vollmer@protonmail.com>
2017-06-15 08:34:37 +0200

    create inspector function (POC)

---
+++ lib.js
@@ -0,0 +1,35 @@
+// this is a proof of concept implementation of a code duplication finder
+// that will find the highest threshold and create an array of reports.
+//
+// using https://github.com/danielstjules/jsinspect to find duplicated code
+
+const child_process = require("child_process")
+
+var results = []
+
+function inspector(file, threshold, callback) {
+  var command = `./node_modules/.bin/jsinspect -t ${threshold} -r json ${file}`
+
+  child_process.exec(command, (err, stdout, stderr) => {
+    var result = JSON.parse(stdout)
+
+    if (result.length === 0) {
+      var report = {
+        highestThreshold: 0,
+        result: null
+      }
+      if (results.length !== 0) {
+        report.highestThreshold = threshold-1
+        report.result = results[results.length-1].data
+        // report.data = resultsm // add all reports to the returned data
+      }
+      callback(report)
+
+    } else {
+      results.push({threshold: threshold, data: result})
+      inspector(file, threshold+1, callback)
+    }
+  })
+}
+
+module.exports = inspector
