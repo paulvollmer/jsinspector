c0dc640390230294764b4c06f649a70c46f83fd3
Paul Vollmer <paul.vollmer@protonmail.com>
2017-06-15 21:26:15 +0200

    Create download script to load some test code

---
+++ fixtures/download.sh
@@ -0,0 +1,5 @@
+echo "download test code"
+git clone --depth 3 git@github.com:robinmoisson/staticrypt.git
+git clone --depth 3 git@github.com:veltman/flubber.git
+git clone --depth 3 git@github.com:vuejs/vue.git
+git clone --depth 3 git@github.com:amark/gun.git
