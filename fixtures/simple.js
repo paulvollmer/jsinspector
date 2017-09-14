c75e0790cf9defeea337a625bf3de610c2537fcb
Paul Vollmer <paul.vollmer@protonmail.com>
2017-09-14 18:28:26 +0200

    format fixture code

--- fixtures/simple.js
+++ fixtures/simple.js
@@ -1,13 +1,13 @@
 var x = 3.14
 
 function foo() {
-  console.log("hello");
-  console.log("world");
-  x = 5;
+  console.log('hello')
+  console.log('world')
+  x = 5
 }
 
 function bar() {
-  console.log("hello");
-  console.log("world");
-  x = 5;
+  console.log('hello')
+  console.log('world')
+  x = 5
 }
