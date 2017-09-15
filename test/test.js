533df4b5b0678c8483c5008fbc047ef85460d956
Paul Vollmer <paul.vollmer@protonmail.com>
2017-09-15 22:56:58 +0200

    use jsinspect lib instead of exec the jsinspect bin

--- test/test.js
+++ test/test.js
@@ -7,8 +7,8 @@ describe('inspector', function() {
   it('simple.js', function(done) {
 
     inspector('./fixtures/simple.js', MIN_THRESHOLD, (report) => {
-      assert.equal(report.highestThreshold, 17)
-      
+      assert.equal(report.highestThreshold, 19)
+
       assert.equal(report.result.length, 1)
       assert.equal(report.result[0].instances.length, 2)
 
